28a77c73b33022e8ab2d5b8fae45743f06812312
Justin Yen <justinyen@systexsoftware.com.tw>
2023-05-15 17:03:18 +0800

    加入專案檔案。

---
+++ Dto/BlobResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace AzureStorageAccountClassLib
+{
+    public class BlobResponseDto
+    {
+        public string? Status { get; set; }
+        public bool Error { get; set; }
+        public BlobDto Blob { get; set; }
+
+        public BlobResponseDto()
+        {
+            Blob = new BlobDto();
+        }
+    }
+}
